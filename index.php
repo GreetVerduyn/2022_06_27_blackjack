fd9bd8a1b8fb6842c3390c94c5561523591e5a81
Greet Verduyn <greetverduyn@proximus.be>
2022-06-27 16:37:23 +0200

    reorganised file

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('./Models/Suit.php');
+require_once('./Models/Card.php');
+require_once('./Models/Deck.php');
+require_once ('./Models/Player.php');
+require_once ('./Models/Blackjack.php');
+
+
+$game=new Blackjack();
+var_dump($game);
+
