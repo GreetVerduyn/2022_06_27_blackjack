adf48e28de952c810656052137f0ecd5db797290
Greet Verduyn <greetverduyn@proximus.be>
2022-06-27 14:21:31 +0200

    added empty methods

--- Player.php
+++ Player.php
@@ -5,4 +5,17 @@ class Player{
 
     private $cards = [];
     private $lost = false;
+    public function hit()
+    {
+    }
+    public function surrender()
+    {
+    }
+    public function getScore()
+    {
+    }
+    public function hasLost()
+    {
+    }
+
 }
