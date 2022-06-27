52003cd653e0c1722af981e2a8730ce1897cef92
Greet Verduyn <greetverduyn@proximus.be>
2022-06-27 15:35:27 +0200

    created constructor in class Blackjack

--- Blackjack.php
+++ Blackjack.php
@@ -4,23 +4,27 @@ declare(strict_types=1);
 
 class Blackjack{
 
-    private $player = new Player()
-    private $dealer = new Player();
-    private $deck = new Deck();
+    private $player;
+    private $dealer;
+    private $deck;
 
     public function getPlayer(){
         return $this->player;
-        }
+    }
 
     public function getDealer(){
         return $this->dealer;
-        }
+    }
 
     public function getDeck(){
         return $this->deck;
-        }
-
-
     }
+    public function __construct($player, $dealer){
+        $this->player = new Player;
+        $this->dealer = new Player;
+        $this->deck = new Deck;
+        $this->deck->shuffle();
+    }
+
 
 }
