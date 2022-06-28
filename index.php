e2ad9d893581c8c90bed15a9182fe6908faf0f25
Greet Verduyn <greetverduyn@proximus.be>
2022-06-28 10:51:39 +0200

    started session + add buttons

--- index.php
+++ index.php
@@ -6,7 +6,44 @@ require_once('./Models/Deck.php');
 require_once ('./Models/Player.php');
 require_once ('./Models/Blackjack.php');
 
-
+session_start();
 $game=new Blackjack();
-var_dump($game);
+$_SESSION['game']=$game;
+
+//var_dump($game);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <title>Blackjack</title>
+</head>
+<body>
+
+
+<form method="post">
+    <button type="submit" name="hit_button"
+           class="button" value="Button1">hit</button>
+    <button type="submit" name="stand_button"
+           class="button" value="Button2">stand</button>
+    <button type="submit" name="surrender_button"
+           class="button" value="Button3">surrender</button>
+</form>
+
+
+<?php
+if(isset($_POST['hit_button'])){
+    $_SESSION['game']->getPlayer()->hit($_SESSION['game']->getDeck());
+}
+if(isset($_POST['stand_button'])) {
+//$game->getPlayer()->
+}
+
+if(isset($_POST['surrender_button'])) {
+    $_SESSION['game']->getPlayer()->surrender();
+}
+?>
+</body>
+</html>
 
